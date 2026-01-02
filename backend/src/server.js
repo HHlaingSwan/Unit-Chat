35841a79e45800c4defa5124b2cba558a242d869
H Hlaing Swan <168323270+HHlaingSwan@users.noreply.github.com>
2026-01-02 12:19:51 +0630

    Change

--- backend/src/server.js
+++ backend/src/server.js
@@ -33,14 +33,6 @@ app.use("/api/auth", authRouter);
 app.use("/api/message", messageRouter);
 app.use("/api/user", userRouter);
 
-// for production mode
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../frontend/dist")));
-  app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "../frontend/dist/index.html"));
-  });
-}
-
 httpServer.listen(port, async () => {
   try {
     await connectingDB(url);
